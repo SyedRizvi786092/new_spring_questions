ff75d4140c8bb6ba6ad26c7f6e3d67dd12cb140f
Aditi Chaurasia <103958459+aditi-chaurasia@users.noreply.github.com>
2026-01-03 09:05:11 -0800

    Delete EventManagment directory

--- EventManagment/src/main/java/com/example/EventManagment/exception/InvalidDateFormatExcption.java
+++
@@ -1,9 +0,0 @@
-package com.example.EventManagment.exception;
-
-public class InvalidDateFormatExcption extends RuntimeException{
-	
-	public InvalidDateFormatExcption(String msg) {
-		super(msg);
-	}
-
-}
